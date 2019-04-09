eade0f2a5b476bdbeb8649a968bc2d06a2ed8827
林强-f <linqiang-f@rt.com>
2019-04-09 14:46:32 +0800

    11

---
+++ src/components/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="about">
+      <a    href="/">关于我们</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about'
+}
+</script>
+<style>
+#about {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
